fd587a75b2b06ce21b040dbbd04072d2b9a02587
ProfiDE <profideprofide@gmail.com>
2026-06-13 18:04:43 +0330

    refactor(client): rename host url variable name

--- client/config.php
+++ client/config.php
@@ -1,4 +1,7 @@
 <?php
 
+// Display errors for debugging (remove in production)
+ini_set('display_errors', 1);
+
 // Host's server.php URL
-$hostUrl = "http://localhost/host2gateway/host/server.php";
+$hostURL = "http://localhost/host2gateway/host/server.php";
