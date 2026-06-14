02e55e04db08be0073d0f2c4f60bf8f5ad54b15c
ProfiDE <profideprofide@gmail.com>
2026-06-14 17:52:35 +0330

    feat(client): log detailed JSON response received from gateway request

--- client/client.php
+++ client/client.php
@@ -39,7 +39,7 @@ if (is_string($response)) {
     $decoded = json_decode($response, true);
     if (json_last_error() === JSON_ERROR_NONE) {
         $responseData = $decoded;
-        echo "Received JSON response.<br>";
+        echo "Received JSON response: '" . json_encode($responseData) . "'<br>";
     }
 }
 
