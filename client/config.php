e16455c41005cb64832ebc68a6cedde8655269c5
ProfiDE <profideprofide@gmail.com>
2026-06-14 16:52:33 +0330

    feat(client): define RSA key files path in client/config.php

--- client/config.php
+++ client/config.php
@@ -8,3 +8,8 @@ $hostURL = "http://localhost/host2gateway/host/server.php";
 
 // Test signature content
 $testSignature = 'test_signature';
+
+// Define file paths for required keys
+$serverPublicKeyFile = __DIR__ . '/keys/server.pem';
+$clientPrivateKeyFile = __DIR__ . '/keys/private.pem';
+$clientPublicKeyFile = __DIR__ . '/keys/public.pem';
